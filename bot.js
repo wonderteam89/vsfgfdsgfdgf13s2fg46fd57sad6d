39e207814aaa571ea1298e06560f7f035dfd0d14
lfffftttttttt hgghghghgfd <41709729+wonderteam89@users.noreply.github.com>
2018-07-27 00:37:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -500,30 +500,27 @@ client.on('message', message => {
     }
 });
 
-         client.on('message', message => {
- if(message.content.startsWith(prefix +"guild")){
-       if(message.author.bot) return;
-        if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-const millis = new Date().getTime() - message.guild.createdAt.getTime();
-const now = new Date();
-dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
-const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
-const days = millis / 1000 / 60 / 60 / 24;
-let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-var embed  = new Discord.RichEmbed()
-.setAuthor(message.guild.name, message.guild.iconURL)
-.addField("Guild ID:", message.guild.id,true)
-.addField("Created On", message.guild.createdAt.toLocaleString(),true)
-.addField("Owned by","**"+ message.guild.owner + "**", true)
-.addField("Members  "+`[${message.guild.memberCount}]` ,`Online : [${message.guild.members.filter(m=>m.presence.status == 'online').size}] \n\ Offline :   [${message.guild.members.filter(m=>m.presence.status == 'offline').size}] \n\ DND :  [${message.guild.members.filter(m=>m.presence.status == 'dnd').size}]\n\ Idle :  [${message.guild.members.filter(m=>m.presence.status == 'idle').size}]`,true)
-.addField('Channels ',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' Text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
-.addField("Region" , message.guild.region,true)
-.addField("Roles",`**[${message.guild.roles.size}]** Role `,true)
-.setColor('#000000')
-message.channel.sendEmbed(embed)
+client.on('message', message => {
+    if(message.content === prefix + 'guild'){
+            const millis = new Date().getTime() - message.member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+    var heg = message.guild;
 
-}
-});
+        const embed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .addField('GuidlOwner',message.guild.owner,true)
+        .addField('Guild ID', message.guild.id,true)
+        .addField('Guild MemberCount', `${message.guild.memberCount}`+` [Online : ${message.guild.members.filter(m=>m.presence.status == 'online').size}]`)
+        .addField('Guild Channels',`\`🔊\` ${message.guild.channels.filter(m => m.type === 'text').size} | `+`\`#\`${message.guild.channels.filter(m => m.type === 'voice').size} `)
+        .addField('Guild RolesCount',` ${message.guild.roles.size} `,true)
+        .addField('Created',`\`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
+        .addField('Guild Region',message.guild.region,true)
+        
+        
+        message.channel.send(embed)
+    }
+})
 
 client.on('message', message => {
     if (message.content.startsWith(prefix + "stats")) {
