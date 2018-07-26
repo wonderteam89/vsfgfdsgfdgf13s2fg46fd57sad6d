6d8f8719e2f5f6813f1914d0a131740439af82b5
lfffftttttttt hgghghghgfd <41709729+wonderteam89@users.noreply.github.com>
2018-07-26 04:09:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+//======================================[Const]======================================
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const bot = new Discord.Client();
@@ -11,6 +12,548 @@ const Canvas = require("canvas");
 const jimp = require('jimp')
 const weather = require('weather-js');
 const prefix = "#";
+
+  
+
+
+
+//======================================[Client]======================================
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+   client.user.setActivity("Wonder 🔰",{type: 'WATCHING'})
+});
+
+client.on('guildMemberAdd', member=> {
+  var guild = '471426842667253761';
+    member.addRole(member.guild.roles.find("name","Clients"));
+    });
+    
+    client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var iiMo = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``` New Dm Mesage ```')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`From : (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("471440739981590528").send({ embed: iiMo });
+    }
+});
+    
+//======================================[Owners]======================================
+
+
+const developers = ["290908670529896448"]
+const admin = "#";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(admin + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  }  else  
+  if (message.content.startsWith(admin + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(admin + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(admin + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(admin + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(admin + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});  
+
+
+//======================================[ Log ]======================================
+
+  client.on('messageUpdate', (message, newMessage) => {
+    if (message.content === newMessage.content) return;
+    if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
+    const channel = message.guild.channels.find('name', 'log');
+    if (!channel) return;
+ 
+    let embed = new Discord.RichEmbed()
+       .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+       .setColor('BLACK')
+       .setDescription(`✏ **Message Editing
+Sent By : <@${message.author.id}>                                                                                                                         Edit In :** <#${message.channel.id}>\n\nOld :\n \`${message.cleanContent}\`\n\nNew :\n \`${newMessage.cleanContent}\``)
+       .setTimestamp();
+     channel.send({embed:embed});
+ 
+ 
+});
+ 
+client.on('guildMemberAdd', member => {
+    if (!member || !member.id || !member.guild) return;
+    const guild = member.guild;
+   
+    const channel = member.guild.channels.find('name', 'log');
+    if (!channel) return;
+    let memberavatar = member.user.avatarURL
+    const fromNow = moment(member.user.createdTimestamp).fromNow();
+    const isNew = (new Date() - member.user.createdTimestamp) < 900000 ? '🆕' : '';
+   
+    let embed = new Discord.RichEmbed()
+       .setAuthor(`${member.user.tag}`, member.user.avatarURL)
+       .setThumbnail(memberavatar)
+       .setColor('GREEN')
+       .setDescription(`📥 <@${member.user.id}> **Joined To The Server**\n\n`)
+       .setTimestamp();
+     channel.send({embed:embed});
+});
+ 
+client.on('guildMemberRemove', member => {
+    if (!member || !member.id || !member.guild) return;
+    const guild = member.guild;
+   
+    const channel = member.guild.channels.find('name', 'log');
+    if (!channel) return;
+    let memberavatar = member.user.avatarURL
+    const fromNow = moment(member.joinedTimestamp).fromNow();
+   
+    let embed = new Discord.RichEmbed()
+       .setAuthor(`${member.user.tag}`, member.user.avatarURL)
+       .setThumbnail(memberavatar)
+       .setColor('RED')
+       .setDescription(`📤 <@${member.user.id}> **Leave From Server**\n\n`)
+       .setTimestamp();
+     channel.send({embed:embed});
+});
+ 
+client.on('messageDelete', message => {
+    if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
+    const channel = message.guild.channels.find('name', 'log');
+    if (!channel) return;
+   
+    let embed = new Discord.RichEmbed()
+       .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+       .setColor('BLACK')
+       .setDescription(`🗑️ **Deleted Message**
+**Sent By : <@${message.author.id}>                                                                                                                        Deleted in :** <#${message.channel.id}>\n\n \`${message.cleanContent}\``)
+       .setTimestamp();
+     channel.send({embed:embed});
+ 
+});
+
+     
+      client.on("roleDelete", role => {
+  client.setTimeout(() => {
+    role.guild.fetchAuditLogs({
+        limit: 1,
+        type: 30
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username)
+        try {
+
+          let log = role.guild.channels.find('name', 'log');
+          if (!log) return;
+          let embed = new Discord.RichEmbed()
+            .setColor('#fd0101')            
+            .setTitle('❌ RoleDeleted')
+            .addField('Role Name', role.name, true)
+            .addField('Role ID', role.id, true)
+            .addField('By', exec, true)
+            .setTimestamp()
+          log.send(embed).catch(e => {
+            console.log(e);
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      })
+  }, 1000)
+})
+
+
+client.on('roleCreate', role => {
+  client.setTimeout(() => {
+    role.guild.fetchAuditLogs({
+        limit: 1,
+        type: 30
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username)
+        try {
+
+          let log = role.guild.channels.find('name', 'log');
+          if (!log) return;
+          let embed = new Discord.RichEmbed()
+            .setTitle('➕ RoleCreated')
+            .addField('Role Name', role.name, true)
+            .addField('Role ID', role.id, true)
+            .addField('By', exec, true)
+            .setTimestamp()
+          log.send(embed).catch(e => {
+            console.log(e);
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      })
+  }, 1000)
+})
+
+
+
+
+  client.on("guildBanAdd", (guild, member) => {
+  client.setTimeout(() => {
+    guild.fetchAuditLogs({
+        limit: 1,
+        type: 22
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username);
+        try {
+          let log = guild.channels.find('name', 'log');
+          if (!log) return;
+          client.fetchUser(member.id).then(myUser => {
+          let embed = new Discord.RichEmbed()
+        .setAuthor("Ban Added")
+	    .setColor("BLACK")
+        .setThumbnail(myUser.avatarURL)
+        .addField('# Banned User:',`**${myUser.username}**`,true)
+        .addField('# Banned By:',`**${exec}**`,true)
+        .setFooter(myUser.username,myUser.avatarURL)
+            .setTimestamp();
+          log.send(embed).catch(e => {
+            console.log(e);
+          });
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  }, 1000);
+});
+
+
+
+    client.on("guildBanRemove", (guild, member) => {
+  client.setTimeout(() => {
+    guild.fetchAuditLogs({
+        limit: 1,
+        type: 22
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username);
+        try {
+          let log = guild.channels.find('name', 'log');
+          if (!log) return;
+          client.fetchUser(member.id).then(myUser => {
+          let embed = new Discord.RichEmbed()
+        .setAuthor("Ban revoked !")
+		.setColor("BLACK")
+		 .setThumbnail(myUser.avatarURL)
+        .addField('# Banned User',`**${myUser.username}**`,true)
+        .addField('# Revoked By',`**${exec}**`,true)
+        .setFooter(myUser.username,myUser.avatarURL)
+            .setTimestamp();
+          log.send(embed).catch(e => {
+            console.log(e);
+          });
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  }, 1000);
+});
+
+
+//======================================[ WelCome ]======================================
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("471426842667253761");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("471426842667253763");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    			 	         var currentTime = new Date(),
+		  hours = currentTime.getHours() + 4 ,
+          hours2 = currentTime.getHours() + 1 ,             
+		   minutes = currentTime.getMinutes(),             
+		   seconds = currentTime.getSeconds(),
+            Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate();
+             if(hours2 > 12) {
+               hours2 -= 12;
+            } else if(hours2 == 0) {
+                hours2 = "12";
+            
+            }  
+            var suffix = 'AM';
+            if (hours >= 12) {
+                suffix = 'PM';
+                hours = hours - 12;	
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+         var ee = member.user;
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("471426842667253761");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(` - Welcome ${member} To Wonder \n\ - Invited By : ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
+client.on('guildMemberAdd', member => {
+var Canvas = require('canvas') //npm i canvas
+var jimp = require('jimp') //npm i jimp
+
+const w = ['Wonder.png'];  
+
+        let Image = Canvas.Image,
+            canvas = new Canvas(401, 202),
+            ctx = canvas.getContext('2d');
+        ctx.patternQuality = 'bilinear';
+        ctx.filter = 'bilinear';
+        ctx.antialias = 'subpixel';
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+        ctx.shadowOffsetY = 2;
+        ctx.shadowBlur = 2;
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+            if (err) return console.log(err);
+            let BG = Canvas.Image;
+            let ground = new Image;
+            ground.src = Background;
+            ctx.drawImage(ground, 0, 0, 401, 202);
+
+})
+
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+
+                        
+                           ctx.font = '40px Arial Bold';
+                        ctx.fontSize = '80px';
+                        ctx.fillStyle = "#FFFFFF";
+                        ctx.textAlign = "center";
+                        ctx.fillText(`${member.guild.memberCount}Th`, 252, 190); //MemberCount
+
+                        //ur name
+                        ctx.font = '25px Arial';
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#000000";
+                        ctx.textAlign = "center";
+                        ctx.fillText(member.user.username, 255, 133); //shows username!
+
+                        
+                        
+                        //Avatar
+                                               //Avatar
+                        let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(100, 100, 80, 0, Math.PI*2);
+                                 ctx.closePath();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 6, 6, 185, 185);
+                                                //wl
+                     
+member.guild.channels.get('471426842667253763').sendFile(canvas.toBuffer());
+
+
+
+
+})
+})
+
+});
+  
+  
+//======================================[Commands]======================================
+  
+  
+  client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("Error : `` Type a Value To Delete ``").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.reply("Cleared : ``" + textxt + "``").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
+client.on('message', message => {
+    if (message.content.startsWith( prefix + "avatar")) {
+        if(message.author.bot) return;
+        if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      
+        const embed = new Discord.RichEmbed()
+        .setColor("BLACK")
+        .setImage(`${x5bzm.avatarURL}`)
+        .addField('Requested By:', message.author.tag)
+      message.channel.sendEmbed(embed);
+      }
+    }
+});
+client.on('message', message => {
+    if (message.content.startsWith( prefix + "avatar")) {
+        if(message.author.bot) return;
+        if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+        var mentionned = message.mentions.users.first();
+
+ if (mentionned) return;
+          var x5bzm = message.author;
+      
+        const embed = new Discord.RichEmbed()
+        .setColor("BLACK")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+         client.on('message', message => {
+ if(message.content.startsWith(prefix +"guild")){
+       if(message.author.bot) return;
+        if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("Guild ID:", message.guild.id,true)
+.addField("Created On", message.guild.createdAt.toLocaleString(),true)
+.addField("Owned by","**"+ message.guild.owner + "**", true)
+.addField("Members  "+`[${message.guild.memberCount}]` ,`Online : [${message.guild.members.filter(m=>m.presence.status == 'online').size}] \n\ Offline :   [${message.guild.members.filter(m=>m.presence.status == 'offline').size}] \n\ DND :  [${message.guild.members.filter(m=>m.presence.status == 'dnd').size}]\n\ Idle :  [${message.guild.members.filter(m=>m.presence.status == 'idle').size}]`,true)
+.addField('Channels ',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' Text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
+.addField("Region" , message.guild.region,true)
+.addField("Roles",`**[${message.guild.roles.size}]** Role `,true)
+.setColor('#000000')
+message.channel.sendEmbed(embed)
+
+}
+});
+
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "stats")) {
+               if(message.author.bot) return;
+        if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setColor('BLACK')
+            .addField('Ping' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('RAM Usage', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('ID' , `[ ${client.user.id} ]` , true)
+            .addField('Prefix' , `[ ${prefix} ]` , true)
+            
+    })
+}
+});
+      
+      const arraySort = require('array-sort'),
+          table = require('table');
+
+client.on('message' , async (message) => {
+
+    if(message.content.startsWith(prefix + "invites")) {
+                 if(message.author.bot) return;
+        if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+
+  var invites = await message.guild.fetchInvites();
+
+    invites = invites.array();
+
+    arraySort(invites, 'uses', { reverse: true });
+
+    let possibleInvites = ['User Invited |  Uses '];
+    invites.forEach(i => {
+        if (i.uses === 0) { 
+            return;
+            
+        }
+      possibleInvites.push(['\n\ ' +'<@'+ i.inviter.id +'>' + '  :  ' +   i.uses]);
+       
+      if (i.uses === 20) {
+          message.member.addRole(message.member.guild.roles.find("name","Clients"));
+      }
+     
+    })
+    
+    const embed = new Discord.RichEmbed()
+    .setColor('BLACK')
+    .addField("Top Invites." ,`${(possibleInvites)}`)
+
+    message.channel.send(embed)
+    }
+});
 
   
 
