6276aaff2aaa949895b10467007062f97848dc52
lfffftttttttt hgghghghgfd <41709729+wonderteam89@users.noreply.github.com>
2018-07-26 02:25:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,19 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const ms = require("ms");
+const fs = require('fs');
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+const r1 = require('snekfetch');
+const Canvas = require("canvas");
+const jimp = require('jimp')
+const weather = require('weather-js');
+const prefix = "#";
 
+  
+
+
+
+client.login(process.env.BOT_TOKEN);
