63620c682a9394f6191d3b7912d084276205cda2
lfffftttttttt hgghghghgfd <41709729+wonderteam89@users.noreply.github.com>
2018-07-30 19:16:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,28 @@ const prefix = "#";
 
   
 
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+          if(!message.channel.guild) return;
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+           .setColor('#36393e')
+        .setDescription(" تم ارسال الرابط بالخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+       .setColor('#36393e')
+        .setDescription(`**  Link : \n MaxUses : 100 **`)
+     
+      message.author.sendEmbed(Embed11)
+    }
+});
 
 
 //======================================[Client]======================================
